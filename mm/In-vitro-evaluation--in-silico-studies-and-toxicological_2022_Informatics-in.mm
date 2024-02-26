3be16023b22c58960861456c6429b80070cc359f
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2024-02-26 23:04:17 +0000

    ignore files

--- mm/In-vitro-evaluation--in-silico-studies-and-toxicological_2022_Informatics-in.mm
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<map version="1.0.1">
-  <node TEXT="topic">
-    <node TEXT="model">
-      <node TEXT="The obtained results of  interactions between xanthones and target enzymes were depicted in  Figs. 4.Discussion  To help meet the critical need for safe and effective anti leishmanial  drugs  several bioactive compounds with antileishmanial activity and  diverse modes of action have been synthetized  36 42 . Herein  we are  reporting effective and safer xanthones derivatives for the inhibition of  Leishmania donovani  employing Amphotericin B as reference  compound. All xanthones showed signif   icant IC50 values  pair sample test  P  0.05  in the range 13.09   4.55  0.075  0.09  g ml when compared to amphotericin B whose IC50  value was 0.13  0.02  g ml  Table 1 . This could be  confirmed by the highest IC50 value that associated with xanthone 5   13.09  4.55  g ml   which has a non functionalized dibutoxy sub  stituents. The resultant high affinity of these xanthones to their target  Fig. 4.2D view of the synthesized xanthones bound to modelled trypanothione reductase." />
-    </node>
-  </node>
-</map>
--- mm/Interaction-pattern-of-aldose-reductase-with---glucogal_2022_Informatics-in-.mm
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<map version="1.0.1">
-  <node TEXT="topic">
-  </node>
-</map>
